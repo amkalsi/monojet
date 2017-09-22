306b4b622882642f52b4d629d4db3018a0a5be52
athompson-tamu <31362409+athompson-tamu@users.noreply.github.com>
2017-09-22 09:16:01 -0500

    update
    now just writes TH1F's to root file

--- monotop_gen_lept.C
+++ monotop_gen_lept.C
@@ -29,10 +29,7 @@
 #include <TParticle.h>
 
 TH1F *h_pt_ratio;
-TH2F *h_p_dist;
-
-TCanvas *c1 = new TCanvas("c1", "Pt ratio plot", 800, 700);
-TCanvas *c2 = new TCanvas("c2", "Transerse and Z Momentum Distribution", 800, 700);
+TH1F *h_ratio_with_vm;
 
 void monotop_gen_lept::Begin(TTree * /*tree*/) {
   TString option = GetOption();
@@ -42,15 +39,11 @@ void monotop_gen_lept::SlaveBegin(TTree * /*tree*/) {
   TString option = GetOption();
 
   // PT ratio.
-  h_pt_ratio = new TH1F("pt_ratio", "Pt ratio for LH monotop, W->mu,vm", 100, 0., 1.);
+  h_pt_ratio = new TH1F("pt_ratio_right", "Pt ratio for RH monotop, W->mu,vm", 100, 0., 1.);
   h_pt_ratio->GetXaxis()->SetTitle("Pt(b)/(Pt(b) + Pt(mu))");
   h_pt_ratio->GetYaxis()->SetTitle("Nb events");
 
-  // Px, Py, Pz distribution
-  h_p_dist = new TH2F("p_dist", "Muon Transerse and Z Momentum Distribution (LH)", 100, 0.,500.,
-                       100, 0., 500.);
-  h_p_dist->GetXaxis()->SetTitle("PT");
-  h_p_dist->GetYaxis()->SetTitle("Pz");
+  h_ratio_with_vm = new TH1F("pt_r_vm_right", "", 100, 0., 1.);
 }
 
 Bool_t monotop_gen_lept::Process(Long64_t entry) {
@@ -62,8 +55,9 @@ Bool_t monotop_gen_lept::Process(Long64_t entry) {
   TLorentzVector mu;
 
   // Loop over particles.
-  Int_t b_i = 0;
-  Int_t mu_i = 0;
+  Int_t b_i = -1;
+  Int_t mu_i = -1;
+  Int_t vm_i = -1;
   for (Int_t i = 0; i<Particle_size; i++) {
     // Store the index of the muon.
     if (abs(Particle_PID[i]) == 13) {
@@ -74,41 +68,50 @@ Bool_t monotop_gen_lept::Process(Long64_t entry) {
     if (abs(Particle_PID[i]) == 5 && Particle_Status[i] == 1) {
       b_i = i;
     }
+
+    // Get the neutrino.
+    if (abs(Particle_PID[i]) == 14) {
+      vm_i = i;
+    }
   }
 
-  // Construct the TLorentzVectors for the b and mu.
-  TLorentzVector b(Particle_Px[b_i], Particle_Py[b_i], Particle_Pz[b_i], Particle_E[b_i]);
-  TLorentzVector mu(Particle_Px[mu_i], Particle_Py[mu_i], Particle_Pz[mu_i], Particle_E[mu_i]);
+  if (vm_i >= 0 && mu_i >= 0 && b_i >= 0) {
+    TLorentzVector b(Particle_Px[b_i], Particle_Py[b_i],
+                     Particle_Pz[b_i], Particle_E[b_i]);
+    TLorentzVector mu(Particle_Px[mu_i], Particle_Py[mu_i],
+                      Particle_Pz[mu_i], Particle_E[mu_i]);
 
-  Double_t pt_ratio = b.Pt() / (b.Pt() + mu.Pt());
-  Double_t delta_phi_bl = abs(b.Phi() - mu.Phi());
-  Double_t b_eta = abs(b.Eta());
+    // Define kinematics.
+    Double_t delta_phi_bl = abs(b.Phi() - mu.Phi());
+    Double_t b_eta = abs(b.Eta());
+    Double_t vm_pt = Particle_PT[vm_i];
+    Double_t pt_ratio = b.Pt() / (b.Pt() + mu.Pt());
+    Double_t pt_ratio_with_vm = b.Pt() / (b.Pt() + mu.Pt() + vm_pt);
 
-  // Make cuts.
-  bool cut_b_pt = (b.Pt() > 70.);
-  bool cut_l_pt = (mu.Pt() > 30.);
-  bool cut_delta_phi = (delta_phi_bl < 1.7);
-  bool cut_eta = (b_eta < 2.5);
-  bool passed_cuts = (cut_b_pt && cut_eta && cut_l_pt && cut_delta_phi);
+    // Make cuts.
+    bool cut_b_pt = (b.Pt() > 70.);
+    bool cut_l_pt = (mu.Pt() > 30.);
+    bool cut_delta_phi = (delta_phi_bl < 1.7);
+    bool cut_eta = (b_eta < 2.5);
+    bool passed_cuts = (cut_b_pt && cut_eta && cut_l_pt && cut_delta_phi);
 
-  if (passed_cuts) {
-    h_pt_ratio->Fill(pt_ratio);
-    h_p_dist->Fill(mu.Pt(), mu.Pz());
-  } else {
-    break;
+    if (passed_cuts) {
+      h_pt_ratio->Fill(pt_ratio);
+      h_ratio_with_vm->Fill(pt_ratio_with_vm);
+    }
   }
 
   return kTRUE;
 }
 
 void monotop_gen_lept::SlaveTerminate() {
-  c1->cd();
-  h_pt_ratio->Draw();
-  c1->Print("pt_ratio_left.png");
+  TFile *f_left_gen = new TFile("pt_hist_right_gen.root", "RECREATE");
+  h_pt_ratio->Write();
+  f_left_gen->Close();
 
-  c2->cd();
-  h_p_dist->Draw("CONT4");
-  c2->Print("p_2d_left.png");
+  TFile *f_left_gen_vm = new TFile("pt_ratio_with_vm_right.root", "RECREATE");
+  h_ratio_with_vm->Write();
+  f_left_gen_vm->Close();
 }
 
 void monotop_gen_lept::Terminate() {}
