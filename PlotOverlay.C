ab0e6f14c1e83649ccf1335fc8df0ae69d1536a9
athompson-tamu <31362409+athompson-tamu@users.noreply.github.com>
2017-09-22 23:59:23 -0500

    Update PlotOverlay.C

--- PlotOverlay.C
+++ PlotOverlay.C
@@ -11,17 +11,19 @@
 // Optional: file1 legend title
 // Optional: file2 legend title
 
-void PlotOverlay(char *file1, char *file2) {
-  gStyle->SetOptStat(0);
+void PlotOverlay(const char *file1, const char *file2) {
+  //gStyle->SetOptStat(0);
+
   TCanvas *c = new TCanvas("c", "PT ratio", 800, 700);
-  TH1F *h1 = new TH1F("", "", 100, 0., 1.);
-  TH1F *h2 = new TH1F("", "", 100, 0., 1.);
+  TH1F *h1 = new TH1F("", "", 10, 0., 1.);
+  TH1F *h2 = new TH1F("", "", 10, 0., 1.);
 
   TFile *f1 = new TFile(file1);
   TIter next(f1->GetListOfKeys());
   TKey *key;
   while ((key = (TKey*)next())) {
     TClass *cl = gROOT->GetClass(key->GetClassName());
+    cout << "Found object: " << key->GetClassName() << endl;
     if (!cl->InheritsFrom("TH1F")) continue;
     h1 = (TH1F*)key->ReadObj();
   }
@@ -31,25 +33,30 @@ void PlotOverlay(char *file1, char *file2) {
   TKey *key2;
   while ((key2 = (TKey*)next2())) {
     TClass *cl2 = gROOT->GetClass(key2->GetClassName());
+    cout << "Found object: " << key2->GetClassName() << endl;
     if (!cl2->InheritsFrom("TH1F")) continue;
     h2 = (TH1F*)key2->ReadObj();
   }
 
-  h1->SetTitle("pT Ratio, inclusive neutrino (leptonic W - GEN level)");
-  h1->GetXaxis()->SetTitle("Pt(b)/(Pt(b) + Pt(mu) + Pt(vm))");
-  h1->GetYaxis()->SetTitle("Nb events");
-  h1->GetYaxis()->SetRangeUser(0., 250.);
-  h1->SetLineColor(kRed);
+  h1->SetTitle("pT Ratio - Pythia Handled W Decay");
+  h1->GetXaxis()->SetTitle("Pt(b)/(Pt(b) + Pt(mu)");
+  h1->GetYaxis()->SetTitle("");
+  h1->GetYaxis()->SetRangeUser(0., .2);
+  
+  h1->SetLineColorAlpha(46, 1);
   h1->SetLineWidth(4);
+  h1->SetFillColorAlpha(46, 0.1);
 
-  h2->SetLineColor(kBlue);
+  h2->SetLineColorAlpha(9, 1);
+  h2->SetFillColorAlpha(9, 0.1);
   h2->SetLineWidth(4);
 
   h1->Draw();
   h2->Draw("SAME");
 
-  TLegend *legend = new TLegend(0.1, 0.65, 0.48, 0.82);
+  TLegend *legend = new TLegend(0.1, 0.65, 0.3, 0.75);
   legend->AddEntry(h1, "Left Handed", "f");
   legend->AddEntry(h2, "Right Handed", "f");
   legend->Draw();
 }
+
